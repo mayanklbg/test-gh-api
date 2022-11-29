5aa3b72e35e5feb91a7bbbe43ae0af6b4ddd6fc7
mayanklbg <mayank.agarwal@lloydsbanking.com>
2022-11-29 22:05:34 +0000

    feat CPE-001, Update docker file

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "project_kube_02_svc_name" {
+  type    = string
+  default = "pdev-mngd-02-kube-02"
+}
+variable "project_atch_02_svc_name" {
+  type    = string
+  default = "pdev-mngd-02-atch-02"
+}
